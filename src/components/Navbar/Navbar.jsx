da07a798188f4c14a325bb45ff3419a218b8701c
Anwar_Hossain <ranakrphone@gmail.com>
2024-10-27 20:57:53 +0600

    navbar design started

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "../Link/Link";
+
+const Navbar = () => {
+  const routes = [
+    { id: 1, path: "/", name: "Home" },
+    { id: 2, path: "/about", name: "About" },
+    { id: 3, path: "/services", name: "Services" },
+    { id: 4, path: "/contact", name: "Contact" },
+    { id: 5, path: "*", name: "NotFound" },
+  ];
+  return (
+    <nav>
+      <ul className="md:flex">
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
