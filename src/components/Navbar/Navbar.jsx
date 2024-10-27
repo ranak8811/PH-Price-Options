b0e01218c5b2e31151e1c8fa459ea7264b6d6ad3
Anwar_Hossain <ranakrphone@gmail.com>
2024-10-27 23:48:24 +0600

    responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "../Link/Link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, path: "/", name: "Home" },
     { id: 2, path: "/about", name: "About" },
@@ -10,8 +12,19 @@ const Navbar = () => {
     { id: 5, path: "*", name: "NotFound" },
   ];
   return (
-    <nav>
-      <ul className="md:flex">
+    <nav className="p-4">
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+        {open ? (
+          <AiOutlineClose></AiOutlineClose>
+        ) : (
+          <AiOutlineMenu className=""></AiOutlineMenu>
+        )}
+      </div>
+      <ul
+        className={`md:flex absolute md:static duration-1000 bg-slate-200 px-6 ${
+          open ? "top-12" : "-top-60"
+        }`}
+      >
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
