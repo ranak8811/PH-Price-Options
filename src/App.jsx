d78fed47eacf21929d3c9ab6a79fbc76ecaee79c
Anwar_Hossain <ranakrphone@gmail.com>
2024-10-28 01:30:02 +0600

    spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import DaisyNav from "./components/DaisyNav/DaisyNav";
+import LineChart from "./components/LineChart/LineChart";
 import Navbar from "./components/Navbar/Navbar";
+import Phones from "./components/Phones/Phones";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Navbar></Navbar>
       <h1 className="text-3xl bg-yellow-200">ami + tmi</h1>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
